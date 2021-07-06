9fdbf554c4dc265d31ddee8cf2ef4fe3838b21ff
bossbaby <3064273390@qq.com>
2021-07-06 20:37:43 +0800

    update localforage db name

--- extend-luogu.user.js
+++ extend-luogu.user.js
@@ -50,13 +50,15 @@ const markdown = window.markdown;
 const MarkdownPalettes = window.MarkdownPalettes;
 const WordCloud = window.WordCloud;
 const mdp = uindow.markdownPalettes;
-const forage = window.localforage;
 const log = (...s) => uindow.console.log("%c[exlg]", "color: #0e90d2;", ...s);
 const warn = (...s) => uindow.console.warn("%c[exlg]", "color: #0e90d2;", ...s);
 const error = (...s) => {
     uindow.console.error("%c[exlg]", "color: #0e90d2;", ...s);
     throw Error(s.join(" "));
 };
+
+const forage = window.localforage;
+forage.config({ name: "Extend Luogu Configs" });
 
 const xss = new filterXSS.FilterXSS({
     onTagAttr: (_, k, v, __) => {
@@ -251,9 +253,9 @@ const mod = {
                 p.startsWith("@tcs1/") && location.host === "service-ig5px5gh-1305163805.sh.apigw.tencentcs.com" ||
                 p.startsWith("@tcs2/") && location.host === "service-psscsax9-1305163805.sh.apigw.tencentcs.com"
             ) && (
-                    p.endsWith("*") && pn.startsWith(pr.slice(0, -1)) ||
+                p.endsWith("*") && pn.startsWith(pr.slice(0, -1)) ||
                     pn === pr
-                )))
+            )))
 
                 if (await exe(m) === false) return;
         }
@@ -485,19 +487,19 @@ mod.regUserTab("user-intro-ins", "主页指令", "main", null, async () => {
         arg = arg.split(/(?<!!)%/g).map((s) => s.replace(/!%/g, "%"));
         const $blog = $($(".user-action").children()[0]);
         switch (ins) {
-            case "html":
-                $e.replaceWith($(`<p>${xss.process(arg[0])}</p>`));
-                break;
-            case "frame":
-                $e.replaceWith($(`<iframe src="https://www.bilibili.com/robots.txt?url=${encodeURI(arg[0])}"`
+        case "html":
+            $e.replaceWith($(`<p>${xss.process(arg[0])}</p>`));
+            break;
+        case "frame":
+            $e.replaceWith($(`<iframe src="https://www.bilibili.com/robots.txt?url=${encodeURI(arg[0])}"`
                     + `style="width: ${arg[1]}; height: ${arg[2]};"></iframe>`
-                ));
-                break;
-            case "blog":
-                if ($blog.text().trim() !== "个人博客") return;
-                $blog.attr("href", arg);
-                $e.remove();
-                break;
+            ));
+            break;
+        case "blog":
+            if ($blog.text().trim() !== "个人博客") return;
+            $blog.attr("href", arg);
+            $e.remove();
+            break;
         }
     });
 }, `
@@ -1009,20 +1011,20 @@ mod.reg("keyboard-and-cli", "键盘操作与命令行", "@/*", async () => {
             /* 当 <action> 为 "enable|disable|toggle"，对名为 <name> 的模块执行对应操作：启用|禁用|切换。当 <action> 为 "save"，保存修改。 */
             const i = mod.find_i(name);
             switch (action) {
-                case "enable":
-                case "disable":
-                case "toggle":
-                    if (i < 0) return cli_error`mod: unknown mod "${name}"`;
-                    const $mod = $($("#exlg-dash-mods").children()[i]).children();
-                    $mod.prop("checked", {
-                        enable: () => true, disable: () => false, toggle: (now) => !now
-                    }[action]($mod.prop("checked"))).trigger("change");
-                    break;
-                case "save":
-                    storage.mod_map = mod.map;
-                    break;
-                default:
-                    return cli_error`mod: unknown action "${action}"`;
+            case "enable":
+            case "disable":
+            case "toggle":
+                if (i < 0) return cli_error`mod: unknown mod "${name}"`;
+                const $mod = $($("#exlg-dash-mods").children()[i]).children();
+                $mod.prop("checked", {
+                    enable: () => true, disable: () => false, toggle: (now) => !now
+                }[action]($mod.prop("checked"))).trigger("change");
+                break;
+            case "save":
+                storage.mod_map = mod.map;
+                break;
+            default:
+                return cli_error`mod: unknown action "${action}"`;
             }
         },
         dash: (action/*!string*/) => {
@@ -1090,27 +1092,27 @@ mod.reg("keyboard-and-cli", "键盘操作与命令行", "@/*", async () => {
 
     $cli_input.on("keydown", (e) => {
         switch (e.key) {
-            case "Enter":
-                if (cli_is_log) return cli_clean();
-                const cmd = $cli_input.val();
-                cli_history.push(cmd);
-                cli_history_index = cli_history.length;
-                parse(cmd);
-                if (!cli_is_log) return cli_clean();
-                break;
-            case "/":
-                if (cli_is_log) cli_clean();
-                break;
-            case "Escape":
-                $cli.hide();
-                break;
-            case "ArrowUp":
-            case "ArrowDown":
-                const i = cli_history_index + { ArrowUp: -1, ArrowDown: +1 }[e.key];
-                if (i < 0 || i >= cli_history.length) return;
-                cli_history_index = i;
-                $cli_input.val(cli_history[i]);
-                break;
+        case "Enter":
+            if (cli_is_log) return cli_clean();
+            const cmd = $cli_input.val();
+            cli_history.push(cmd);
+            cli_history_index = cli_history.length;
+            parse(cmd);
+            if (!cli_is_log) return cli_clean();
+            break;
+        case "/":
+            if (cli_is_log) cli_clean();
+            break;
+        case "Escape":
+            $cli.hide();
+            break;
+        case "ArrowUp":
+        case "ArrowDown":
+            const i = cli_history_index + { ArrowUp: -1, ArrowDown: +1 }[e.key];
+            if (i < 0 || i >= cli_history.length) return;
+            cli_history_index = i;
+            $cli_input.val(cli_history[i]);
+            break;
         }
     });
 
