b990e3a8915b5f5c8def9b71e7cdc44a0337f39d
bossbaby <3064273390@qq.com>
2021-07-07 11:06:45 +0800

    update name

--- extend-luogu.user.js
+++ extend-luogu.user.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name           extend-luogu
+// @name           Extend Luogu+
 // @namespace      http://tampermonkey.net/
 // @version        6.2.6
 // @description    Make Luogu more powerful.
